a3688e895b21a4012c7dd134792e3afe94b9d62c
Connor Adams <connorvadams@gmail.com>
2023-12-07 10:05:14 -0700

    switch to artist_to_artist

--- server/models/Artist.py
+++ server/models/Artist.py
@@ -3,6 +3,12 @@ from sqlalchemy.ext.associationproxy import association_proxy
 
 from models.like import Like
 
+artist_to_artist = db.Table(
+    'artist_to_artist',
+    db.Column('followed_artist_id', db.Integer, db.ForeignKey('artists.id'), primary_key=True),
+    db.Column('following_artist_id', db.Integer, db.ForeignKey('artists.id'), primary_key=True),
+)
+
 class Artist(db.Model):
     __tablename__ = 'artists'
 
@@ -17,10 +23,30 @@ class Artist(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    # Other artists that this artist follows
+    followed_artists = db.relationship(
+        'Artist',
+        secondary=artist_to_artist,
+        primaryjoin=id == artist_to_artist.c.following_artist_id,
+        secondaryjoin=id == artist_to_artist.c.followed_artist_id,
+        backref=db.backref('following_artists', lazy='dynamic'),
+        lazy='dynamic'
+    )
+
+    # Followers of this artist
+    following_artists = db.relationship(
+        'Artist',
+        secondary=artist_to_artist,
+        primaryjoin=id == artist_to_artist.c.followed_artist_id,
+        secondaryjoin=id == artist_to_artist.c.following_artist_id,
+        backref=db.backref('followed_artists', lazy='dynamic'),
+        lazy='dynamic'
+    )
+
     # Relationships for an artist's own content
-    own_events = db.relationship('Event', back_populates='artist', cascade='all, delete-orphan')
-    own_tracks = db.relationship('Track', back_populates='artist', cascade='all, delete-orphan')
-    fans = db.relationship('Fan', back_populates='artists')
+    # own_events = db.relationship('Event', back_populates='artist', cascade='all, delete-orphan')
+    # own_tracks = db.relationship('Track', back_populates='artist', cascade='all, delete-orphan')
+    # fans = db.relationship('Fan', back_populates='artists')
 
     # Relationships for content that an artist likes
     # following = db.relationship(
--- server/seed.py
+++ server/seed.py
@@ -2,6 +2,8 @@
 
 # Standard library imports
 from random import randint, choice as rc
+from datetime import datetime
+from random import choice, sample
 
 # Remote library imports
 from faker import Faker
@@ -9,7 +11,7 @@ from faker import Faker
 # Local imports
 from app import app
 from app_setup import db
-from models.artist import Artist
+from models.artist import Artist, artist_to_artist
 from models.fan import Fan
 from models.like import Like
 from models.track import Track
@@ -17,12 +19,51 @@ from models.band_member import BandMember
 
 fake = Faker()
 
-a1 = Artist(name='test name', genres='test genres', bio='test bio', location='test location')
+def create_artists():
+    artists = []
+    for i in range(1, 11):
+        artist = Artist(
+            name=fake.word().title(),
+            genres=choice(['Rock', 'Pop', 'Hip-Hop', 'Jazz', 'Electronic', 'Country', 'R&B', 'Classical']),
+            bio=fake.text(max_nb_chars=200),
+            location=fake.city(),
+            created_at=datetime.now(),
+            updated_at=datetime.now()
+        )
+        artists.append(artist)
+    return artists
+
+def create_artist_follows():
+    # Fetch all artist IDs from the database
+    all_artist_ids = [artist.id for artist in Artist.query.all()]
+
+    # Generate 25 random pairs of followed and following artist IDs
+    pairs_of_artist_ids = sample(all_artist_ids, k=50)  # k=50 since each row contains two IDs
+
+    # Create rows for the artist_to_artist table
+    artist_follows = [
+        {'followed_artist_id': pairs_of_artist_ids[i], 'following_artist_id': pairs_of_artist_ids[i + 1]}
+        for i in range(0, len(pairs_of_artist_ids), 2)
+    ]
+
+    return artist_follows
 
 if __name__ == '__main__':
     fake = Faker()
     with app.app_context():
+        print('Clearing db...')
+        Artist.query.delete()
+        db.session.query(artist_to_artist).delete()
+        db.session.commit()
+
         print("Starting seed...")
-        db.session.add(a1)
+        artists = create_artists()
+        db.session.add_all(artists)
+        db.session.commit()
+
+        artist_follows = create_artist_follows()
+        for row_data in artist_follows:
+            association_row = artist_to_artist.insert().values(row_data)
+            db.session.execute(association_row)
         db.session.commit()
         
