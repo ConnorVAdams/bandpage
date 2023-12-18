5b179dff7d03a132bd30b8dce812f91a070d7d60
Connor Adams <connorvadams@gmail.com>
2023-12-17 20:32:01 -0700

    auth complete, now onto access tokens

--- client/src/features/spotify/SpotifyAuth.js
+++ client/src/features/spotify/SpotifyAuth.js
@@ -1,41 +1,25 @@
-import { useState } from 'react'
 
 const SpotifyAuth = () => {
-  const [url, setUrl] = useState(null)
-
   const handleAuthorize = async () => {
     try {
       const response = await fetch('/authorize', {
         method: 'GET',
-        // You can include headers or other options if needed
       });
 
-      // Check if the request was successful (status code 200-299)
       if (response.ok) {
-        // Extract the 'location' header from the response
         const locationHeader = response.headers.get('location');
 
-        // Update the 'url' state with the extracted URL
-        setUrl(locationHeader);
-
-        // Open a new tab with the extracted URL
         if (locationHeader) {
-          window.open(locationHeader, '_blank');
+          // Redirect the current tab to the Spotify authorization URL
+          window.location.href = locationHeader;
         }
-
-        // Handle the successful response here
-        console.log('Authorization request successful');
       } else {
-        // Handle errors for non-2xx status codes
         console.error('Authorization request failed');
       }
     } catch (error) {
-      // Handle network errors or other exceptions
       console.error('Error during authorization request:', error);
     }
   };
-
-  console.log(url)
 
   return (
     <div>
