dbdfdd6211cf36afd45d4deafeea84c83195b493
Connor Adams <connorvadams@gmail.com>
2023-12-23 17:19:33 -0700

    test landing

--- server/app.py
+++ server/app.py
@@ -267,10 +267,10 @@ def handle_404(error):
     return response, error.code
 
 @app.route("/")
-@app.route("/artists")
-@app.route('/fans')
-@app.route('/landing')
-@app.route('/spotify_prof')
+# @app.route("/artists")
+# @app.route('/fans')
+# @app.route('/landing')
+# @app.route('/spotify_prof')
 def index(id=0):
     return render_template("index.html")
 
