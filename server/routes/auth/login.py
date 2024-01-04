a49ae550830ebf2d570d5dc1915a78fefbb8d5d9
Connor Adams <connorvadams@gmail.com>
2024-01-03 18:46:34 -0700

    test fetchpostartist

--- server/routes/auth/login.py
+++ server/routes/auth/login.py
@@ -11,17 +11,19 @@ from schemas.user_schema import UserSchema
 user_schema = UserSchema(session=db.session)
 
 class Login(Resource):
+    print('/login')
     def post(self):
         try:
             data = request.get_json()
             print(data)
             username = data.get('username')
             user = User.query.filter_by(username=username).first()
-            if user and user.authenticate(data.get("password")):
-                jwt = create_access_token(identity=user.id)
-                refresh_token = create_refresh_token(identity=user.id)
-                serialized_user = user_schema.dump(user)
-                return make_response({"user": serialized_user, "jwt_token": jwt, "refresh_token": refresh_token}, 200)
+            # if user and user.authenticate(data.get("password")):
+            #     jwt = create_access_token(identity=user.id)
+            #     refresh_token = create_refresh_token(identity=user.id)
+            #     serialized_user = user_schema.dump(user)
+            #     return make_response({"user": serialized_user, "jwt_token": jwt, "refresh_token": refresh_token}, 200)
+            return 'user', 200
             return {"message": "Username or password incorrect."}, 403
         except Exception as e:
             return {"message": "Username or password incorrect."}, 403
